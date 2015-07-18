728651d3fe96d048d41086a4ab86981831d73d25
Caitlyn Chen <caitlynachen@gmail.com>
2015-07-18 16:23:05 -0700

    added air pump vc

---
+++ AngelHacksCaitlyn/AngelHacksCaitlyn/View Controllers/AirPumpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AirPumpViewController.swift
+//  AngelHacksCaitlyn
+//
+//  Created by Caitlyn Chen on 7/18/15.
+//  Copyright (c) 2015 Caitlyn Chen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class AirPumpViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
