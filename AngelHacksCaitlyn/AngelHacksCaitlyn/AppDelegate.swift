a767ebb64d40abfbc0e87842c3f59db30f2de21a
Adams Ombonga <adamsombonga@gmail.com>
2015-07-19 00:48:54 -0700

    Updated with IBM Bluemix REST Requets

--- AngelHacksCaitlyn/AngelHacksCaitlyn/AppDelegate.swift
+++ AngelHacksCaitlyn/AngelHacksCaitlyn/AppDelegate.swift
@@ -8,7 +8,6 @@
 //
 
 import UIKit
-import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,8 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-         Parse.setApplicationId("PDi8ouoYg1GTQZXgFF2ZvqjLaxfVjOqTVyZxhCmn", clientKey: "dMW3dBqsQfuwU9HrhJNxTSQOUfnfuJNI0nsUJnD8")
-        
         return true
     }
 
